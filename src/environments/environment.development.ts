5380f517ee3ca7a79fb6abff12ec1a8fcb634fec
JD <jdmasterofbusiness@gmail.com>
2026-04-14 17:40:39 +0100

    Configuración firebase

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: "screen-and-eat",
+    appId: "1:790009746135:web:67a02c5e3e482a98f39692",
+    databaseURL: "https://screen-and-eat-default-rtdb.europe-west1.firebasedatabase.app",
+    storageBucket: "screen-and-eat.firebasestorage.app",
+    apiKey: "AIzaSyCvHvyB8-b2QZAr6Pmc_qGGwhBmOP0FROA",
+    authDomain: "screen-and-eat.firebaseapp.com",
+    messagingSenderId: "790009746135",
+    measurementId: "G-HXXMDMH27R",
+    projectNumber: "790009746135",
+    version: "2"
+  }
+}
